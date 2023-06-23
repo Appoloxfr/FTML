f3e35b39af8114c10892584d5fd5816886168175
Gabriel Huet de Froberville <gabriel.huet-de-froberville@epita.fr>
2023-06-23 11:07:47 +0200

    feat(script): Update script to work on poetry env

--- scripts/generate_output_cells.sh
+++ scripts/generate_output_cells.sh
@@ -5,4 +5,15 @@
 # Run all notebooks to generate the output
 # Also generate the HTML output
 
-find . -name "*.ipynb" -print -exec jupyter nbconvert --to notebook --execute {} \; -exec jupyter nbconvert --to html {} \;
+# if using poetry
+ poetry env info -p &> /dev/null
+
+if [ $? -eq 0 ]; then
+    echo "Using poetry"
+    poetry run jupyter nbconvert --to notebook --execute --inplace **/*.ipynb
+    poetry run jupyter nbconvert --to html **/*.ipynb
+else
+    echo "Using conda"
+    jupyter nbconvert --to notebook --execute --inplace **/*.ipynb
+    jupyter nbconvert --to html **/*.ipynb
+fi
